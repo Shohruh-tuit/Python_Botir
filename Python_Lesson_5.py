1de3849d08a9544e9b1ee1c5271fd1a74c6274a2
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-06-23 21:11:33 +0500

    Python

--- Python_Lesson_5.py
+++ Python_Lesson_5.py
@@ -8,10 +8,14 @@
 # soz = 'I love programming with python'
 # print(len(soz.split()))
 
-class Data(object):
-    def __str__(self):
-        return 'str'
-    def __repr__(self):
-        return 'repr'
+# class Data(object):
+#     def __str__(self):
+#         return 'str'
+#     def __repr__(self):
+#         return 'repr'
 
-print('{0!s} {0!r}'.format(Data()))
+# print('{0!s} {0!r}'.format(Data()))
+
+print('{:10}{}'.format('test','ss'))  # testdan keyin 10 ta qator tashlaydi
+print('{:>10} {}'.format('test','ss'))  # testdan oldin 10 ta qator tashlaydi
+print('{:^10}'.format('test'))          # testni 10 beligini ichiga joylashtirib beradi
