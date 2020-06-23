93c13458f23cf6eacdc66435c9091394d7444cfd
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-06-23 13:14:12 +0500

    String

--- Python_Lesson_5.py
+++ Python_Lesson_5.py
@@ -8,3 +8,10 @@
 # soz = 'I love programming with python'
 # print(len(soz.split()))
 
+class Data(object):
+    def __str__(self):
+        return 'str'
+    def __repr__(self):
+        return 'repr'
+
+print('{0!s} {0!r}'.format(Data()))
