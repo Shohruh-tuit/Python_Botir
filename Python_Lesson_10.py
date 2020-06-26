c2cf9e233ae07da47ab9161540e92187edc661c1
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-06-26 23:23:29 +0500

    While

---
+++ Python_Lesson_10.py
@@ -0,0 +1,29 @@
+# son = 1
+# while son <= 31:
+#     print(son)
+#     son += 1                    
+
+                                    # promokod dasturi
+
+# from random import randint
+
+# kodlar = [135, 132, 134]   #list
+# yangi_kod = randint(130, 135)
+
+# i = 0
+# while yangi_kod in kodlar:
+#     i += 1
+#     yangi_kod = randint(130, 135)
+
+# print('Takrorlanishlar soni', i)
+# print(yangi_kod)
+
+
+                                    # Homework qayta qayta zapros
+
+while True:
+    number = input("Butun son kiriting: ")
+    if number.isdigit():
+        print("Rahmat")
+        break
+    print("Xatolik!!!")
